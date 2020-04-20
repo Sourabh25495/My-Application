a08f187c160b87d138652534256852319a2683cb
Sourabh Kulkarni <sourabhuk254@gmail.com>
2020-04-20 16:54:17 -0400

    Server setup (#5)
    * check git access
    
    * test side bar
    
    * Nav bar dummy
    
    * Nav bar dummy
    
    * Mini varient Nav dummy
    
    * Fix conflicts
    
    * routes and server setup

--- src/PortfolioApplication.js
+++ src/PortfolioApplication.js
@@ -17,6 +17,16 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange } from '@material-ui/core/colors';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Profile from './pages/Profile'
 
 
 
@@ -84,18 +94,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const  PortfolioApplication = (props) => {
+const PortfolioApplication = (props) => {
   const { container } = props;
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
     setOpen(true);
   };
 
   const handleDrawerClose = () => {
-    setOpen(false);
+    setOpen(true);
   };
 
   return (
@@ -120,73 +130,80 @@ const  PortfolioApplication = (props) => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Mini variant drawer
+            Dr. Sourabh Kulkarni
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+      <Router>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <Link to="/">
+              <ListItem button>
+                <ListItemIcon>{<Avatar src="" />}</ListItemIcon>
+                <ListItemText primary={'My Profile'} />
+              </ListItem>
+            </Link>
+            <Link to="/education">
+            <ListItem button>
+              <ListItemIcon>{<InboxIcon />}</ListItemIcon>
+              <ListItemText primary={'Education'} />
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            </Link>
+            <Link to="/work-exp">
+              <ListItem button>
+                <ListItemIcon>{<MailIcon />}</ListItemIcon>
+                <ListItemText primary={'Work Experience'} />
+              </ListItem>
+            </Link>
+            <Link to="/projects">
+            <ListItem button>
+              <ListItemIcon>{<InboxIcon />}</ListItemIcon>
+              <ListItemText primary={'Personal Projects'} />
             </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </main>
+            </Link>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography paragraph>
+            <Switch>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path="/education">
+                users
+              </Route>
+              <Route path="/work-exp">
+                Work Exp
+              </Route>
+              <Route path="/projects">
+                Personal projects
+              </Route>
+            </Switch>
+          </Typography>
+          <Typography paragraph>
+
+          </Typography>
+        </main>
+      </Router>
     </div>
   );
 }
--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PortfolioApplication from './PortfolioApplication'
-
+import { HashRouter } from "react-router-dom";
+import PortfolioApplication from "./PortfolioApplication";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
+import Routes from "./Routes";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <PortfolioApplication />
+  return (
+    <div>
+      <Router>
+        <Routes />
+        <PortfolioApplication />
+      </Router>
+    </div>
+  );
 };
+
+//export default withRouter(App)
 
 ReactDOM.render(<App />, document.getElementById("index"));
