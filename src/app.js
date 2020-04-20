8c1001c2f5815f555092d911d7420fd5210963af
YSourabh Kulkarni <sourabhuk254@gmail.com>
2020-04-19 20:57:21 -0400

    initial commit -webpack and bable setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Hello React! Lets start making our portfolio</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
